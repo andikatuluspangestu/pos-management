1f12f09518d201ef3be1f9fa96b72586d2e3894a
andikatuluspangestu <andikatulusp@gmail.com>
2023-11-07 09:53:20 +0700

    Menambah Default Akun (Admin, Customer, Sales)

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rute untuk menampilkan formulir login
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+
+// Rute untuk memproses login
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rute untuk menampilkan formulir registrasi
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+
+// Rute untuk memproses registrasi
+Route::post('/register', [AuthController::class, 'register']);
+
+// Rute untuk logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Rute untuk halaman dashboard (contoh)
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth'); // Hanya dapat diakses oleh pengguna yang sudah masuk
