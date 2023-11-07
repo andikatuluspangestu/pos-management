2fc43b7405b6795dd0506eaa4215a2ea9aca1fa8
andikatuluspangestu <andikatulusp@gmail.com>
2023-11-07 09:51:47 +0700

    Menyesuaikan Model dengan Tabel Users

--- app/User.php
+++ app/User.php
@@ -2,38 +2,24 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'level',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    // Metode untuk mendapatkan level pengguna
+    public function getLevelAttribute($value)
+    {
+        return ucfirst($value);
+    }
 }
