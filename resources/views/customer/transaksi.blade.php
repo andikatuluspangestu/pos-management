d4891fd37562a607cd5c57acdc62224c84823b91
anharsaja <anharmuk12@gmail.com>
2023-12-07 21:49:38 +0700

    customer views update

--- resources/views/customer/transaksi.blade.php
+++ resources/views/customer/transaksi.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid">
 
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Pembelian</h1>
+        <h1 class="h3 mb-0 text-gray-800">Transaksi</h1>
     </div>
 </div>
 @endsection
