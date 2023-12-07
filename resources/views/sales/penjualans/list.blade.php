d940a174c5c5059f92fe042da9f5a63b55c1268a
hafizhulqisthim <ijuljolali@gmail.com>
2023-12-07 17:09:13 +0700

    modifying list at sales/penjualans #1

--- resources/views/sales/penjualans/list.blade.php
+++ resources/views/sales/penjualans/list.blade.php
@@ -6,11 +6,6 @@
     <div class="table-responsive shadow-sm p-3">
       <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
 
-        <button type="button" class="btn btn-sm btn-primary mb-3" data-toggle="modal" data-target="#addCategoryModal">
-          <i class="fas fa-plus"></i>
-          Tambah Kategori
-        </button>
-
         @if(session('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
           {{ session('success') }}
@@ -22,22 +17,25 @@
 
         <thead>
           <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Nama Kategori</th>
+            <th scope="col">ID Penjualan</th>
+            <th scope="col">ID User</th>
+            <th scope="col">Total Item</th>
+            <th scope="col">Total Harga</th>
+            <th scope="col">Status Bayar</th>
             <th scope="col">Aksi</th>
           </tr>
         </thead>
         <tbody>
-          @foreach($data as $category)
+          @foreach($data as $penjualan)
           <tr>
-            <td>{{ $category->category_id }}</td>
-            <td>{{ $category->category_name }}</td>
+            <td>{{ $penjualan->category_id }}</td>
+            <td>{{ $penjualan->category_name }}</td>
             <td>
-              <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editCategoryModal{{ $category->category_id }}">
+              <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editCategoryModal{{ $penjualan->category_id }}">
                 <i class="fas fa-edit"></i>
                 Edit
               </button>
-              <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteCategoryModal{{ $category->category_id }}">
+              <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteCategoryModal{{ $penjualan->category_id }}">
                 <i class="fas fa-trash"></i>
                 Delete
               </button>
@@ -48,49 +46,29 @@
       </table>
     </div>
 
-    @foreach($data as $category)
-    <!-- Delete Category Modal -->
-    <div class="modal fade" id="deleteCategoryModal{{ $category->category_id }}" tabindex="-1" aria-labelledby="deleteCategoryModal{{ $category->category_id }}" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="deleteCategoryModal{{ $category->category_id }}">Hapus Data Kategori</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            Apakah Anda yakin ingin menghapus kategori {{ $category->category_name }}?
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-            <a href="{{ route('categories.delete', $category->category_id) }}" class="btn btn-danger">Hapus</a>
-          </div>
-        </div>
-      </div>
-    </div>
+    @foreach($data as $penjualan)
 
     <!-- Edit Category Modal -->
-    <div class="modal fade" id="editCategoryModal{{ $category->category_id }}" tabindex="-1" aria-labelledby="editCategoryModal{{ $category->category_id }}" aria-hidden="true">
+    <div class="modal fade" id="editCategoryModal{{ $penjualan->category_id }}" tabindex="-1" aria-labelledby="editCategoryModal{{ $penjualan->category_id }}" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title{{ $category->category_id }}" id="editCategoryModal{{ $category->category_id }}">Edit Data Kategori</h5>
+            <h5 class="modal-title{{ $penjualan->category_id }}" id="editCategoryModal{{ $penjualan->category_id }}">Edit Data Kategori</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
-            <form action="{{ route('categories.update', ['id' => $category->category_id]) }}" method="post">
+            <form action="{{ route('categories.update', ['id' => $penjualan->category_id]) }}" method="post">
               @csrf
               @method('PUT')
               <div class="form-group">
                 <label for="category_name">Nama Kategori</label>
-                <input type="text" class="form-control" id="category_name" name="category_name" placeholder="Masukkan Nama Kategori" value="{{ $category->category_name }}">
+                <input type="text" class="form-control" id="category_name" name="category_name" placeholder="Masukkan Nama Kategori" value="{{ $penjualan->category_name }}">
               </div>
               <div class="form-group">
                 <label for="category_description">Deskripsi Kategori</label>
-                <textarea class="form-control" id="category_description" name="category_description" rows="3">{{ $category->category_description }}</textarea>
+                <textarea class="form-control" id="category_description" name="category_description" rows="3">{{ $penjualan->category_description }}</textarea>
               </div>
           </div>
           <div class="modal-footer">
@@ -103,38 +81,6 @@
     </div>
 
     @endforeach
-
-    <!-- Add Category Modal -->
-    <div class="modal fade" id="addCategoryModal" tabindex="-1" aria-labelledby="addCategoryModal" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="addCategoryModal">Tambah Data Kategori</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form action="{{ route('categories.insert') }}" method="POST">
-              @csrf
-              <div class="form-group">
-                <label for="category_name">Nama Kategori</label>
-                <input type="text" class="form-control" id="category_name" name="category_name" placeholder="Masukkan Nama Kategori">
-              </div>
-              <div class="form-group">
-                <label for="category_description">Deskripsi Kategori</label>
-                <textarea class="form-control" id="category_description" name="category_description" rows="3"></textarea>
-              </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-            <button class="btn btn-primary" type="submit">Submit</button>
-          </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
 
 </div>
 @endsection
