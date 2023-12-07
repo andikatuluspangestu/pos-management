6810cc282233ac032271ab80a299d3510bcfd7c8
anharsaja <anharmuk12@gmail.com>
2023-12-07 21:27:40 +0700

    customer view products

--- resources/views/customer/categories.blade.php
+++ resources/views/customer/categories.blade.php
@@ -1,10 +1,84 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid px-5">
+    <div class="row">
+        <div class="table-responsive shadow-sm p-3">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
 
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Categories</h1>
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nama Kategori</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($data as $category)
+                    <tr>
+                        <td>{{ $category->category_id }}</td>
+                        <td>{{ $category->category_name }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        @foreach($data as $category)
+        <!-- Delete Category Modal -->
+        <div class="modal fade" id="deleteCategoryModal{{ $category->category_id }}" tabindex="-1" aria-labelledby="deleteCategoryModal{{ $category->category_id }}" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="deleteCategoryModal{{ $category->category_id }}">Hapus Data Kategori</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Apakah Anda yakin ingin menghapus kategori {{ $category->category_name }}?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                        <a href="{{ route('categories.delete', $category->category_id) }}" class="btn btn-danger">Hapus</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Edit Category Modal -->
+        <div class="modal fade" id="editCategoryModal{{ $category->category_id }}" tabindex="-1" aria-labelledby="editCategoryModal{{ $category->category_id }}" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title{{ $category->category_id }}" id="editCategoryModal{{ $category->category_id }}">Edit Data Kategori</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('categories.update', ['id' => $category->category_id]) }}" method="post">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label for="category_name">Nama Kategori</label>
+                                <input type="text" class="form-control" id="category_name" name="category_name" placeholder="Masukkan Nama Kategori" value="{{ $category->category_name }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="category_description">Deskripsi Kategori</label>
+                                <textarea class="form-control" id="category_description" name="category_description" rows="3">{{ $category->category_description }}</textarea>
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                        <button class="btn btn-primary" type="submit">Submit</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        @endforeach
     </div>
+
 </div>
 @endsection
