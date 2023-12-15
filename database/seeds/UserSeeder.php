fe81cd8252cfcf5860c67bb71fc1962b324db152
andikatuluspangestu <andikatulusp@gmail.com>
2023-12-15 16:21:59 +0700

    Added : More Data Dummy

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,25 +12,49 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // Administrators
         User::create([
-            'name' => 'Administrator',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('12345678'),
-            'role' => 'admin'
+            'name'      => 'Administrator',
+            'email'     => 'admin@example.com',
+            'password'  => bcrypt('12345678'),
+            'role'      => 'admin'
         ]);
 
         User::create([
-            'name' => 'Sales',
-            'email' => 'sales@example.com',
-            'password' => bcrypt('12345678'),
-            'role' => 'sales'
+            'name'      => 'Andika Tulus Pangestu',
+            'email'     => 'andikatulusp@example.com',
+            'password'  => bcrypt('12345678'),
+            'role'      => 'admin'
+        ]);
+
+        // Sales
+        User::create([
+            'name'      => 'Sales',
+            'email'     => 'sales@example.com',
+            'password'  => bcrypt('12345678'),
+            'role'      => 'sales'
         ]);
 
         User::create([
-            'name' => 'Customer',
-            'email' => 'customer@example.com',
-            'password' => bcrypt('12345678'),
-            'role' => 'customer'
+            'name'      => 'Hafizhul Qisti',
+            'email'     => 'hafizh@example.com',
+            'password'  => bcrypt('12345678'),
+            'role'      => 'sales'
+        ]);
+
+        // Customers
+        User::create([
+            'name'      => 'Customer',
+            'email'     => 'customer@example.com',
+            'password'  => bcrypt('12345678'),
+            'role'      => 'customer'
+        ]);
+
+        User::create([
+            'name'      => 'Yulianti Putri',
+            'email'     => 'yulianti@example.com',
+            'password'  => bcrypt('12345678'),
+            'role'      => 'customer'
         ]);
     }
 }
