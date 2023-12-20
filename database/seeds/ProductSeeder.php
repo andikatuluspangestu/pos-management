3831af8c2cae81a0c5d8ac822f16b0034a5c154a
hafizhulqisthim <ijuljolali@gmail.com>
2023-12-20 10:43:51 +0700

    adding another array at ProductSeeder

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -30,23 +30,63 @@ class ProductSeeder extends Seeder
            'gulai.jpeg',
         ];
 
+        $produk_description = [
+           'Bumbu Masak Umik varian rawon yang khusus dibuat untuk memasak beragam masakan',
+           'Bumbu Masak Umik varian sambal goreng yang khusus dibuat untuk memasak beragam masakan',
+           'Bumbu Masak Umik varian nasi goreng yang khusus dibuat untuk memasak beragam masakan',
+           'Bumbu Masak Umik varian sayur sop yang khusus dibuat untuk memasak beragam masakan',
+           'Bumbu Masak Umik varian opor yang khusus dibuat untuk memasak beragam masakan',
+           'Bumbu Masak Umik varian gulai yang khusus dibuat untuk memasak beragam masakan',
+        ];
+
+        $diskon = [
+           10,
+           5, 
+           10,
+           5,
+           10,
+           5, 
+        ];
+
+        $harga_jual = [
+           8000,
+           9000, 
+           10000,
+           8000,
+           9000,
+           10000, 
+        ];
+
+        $stok = [
+           10,
+           11, 
+           12,
+           10,
+           11,
+           12, 
+        ];
+
         $kodeproduk = 1111; 
         foreach ($produks as $index => $produk) {
 
           // mengambil nama file gambar berdasarkan index yang sama
-          $gambarproduk = $gambar[$index]; 
+          $gambarproduk                 = $gambar[$index]; 
+          $produk_descriptionproduk     = $produk_description[$index]; 
+          $diskonproduk                 = $diskon[$index];
+          $harga_jualproduk             = $harga_jual[$index];
+          $stokproduk                   = $stok[$index];
 
           DB::table('tbl_products')->insert([
-             'category_id' => 1,
-                'kode_produk' => $kodeproduk,
-                'nama_produk' => $produk,
-                'gambar' => $gambarproduk,
-                'produk_description' => 'Bumbu Masak Umik varian yang khusus dibuat untuk memasak beragam masakan',
-                'diskon' => 0.05,
-                'harga_jual' => 8000,
-                'stok' => 10,
-                'created_at' => now(),
-                'updated_at' => now(),
+             'category_id'              => 1,
+                'kode_produk'           => $kodeproduk,
+                'nama_produk'           => $produk,
+                'gambar'                => $gambarproduk,
+                'produk_description'    => $produk_descriptionproduk,
+                'diskon'                => $diskonproduk,
+                'harga_jual'            => $harga_jualproduk,
+                'stok'                  => $stokproduk,
+                'created_at'            => now(),
+                'updated_at'            => now(),
           ]);
           $kodeproduk += 1;
         }
