8e8be06c3355b3e85f6332fbe71447eb90ac3c29
andikatuluspangestu <andikatulusp@gmail.com>
2023-12-25 17:32:03 +0700

    Fixed : Error input kode_produk sama

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -31,15 +31,39 @@ class ProdukController extends Controller
             'category_id'           => $request->category_id,
             'kode_produk'           => $request->kode_produk,
             'nama_produk'           => $request->nama_produk,
-            'gambar'                => $request->file('gambar')->storeAs('', basename($request->file('gambar')->getClientOriginalName())),
             'produk_description'    => $request->produk_description,
             'diskon'                => $request->diskon,
             'harga_jual'            => $request->harga_jual,
             'stok'                  => $request->stok,
         ];
 
+        // Cek apakah ada gambar yang diupload
+        if ($request->hasFile('gambar')) {
+            
+            // Konfigurasi folder upload dan nama file
+            $destination = 'uploads';
+            $file_name   = date('YmdHis') . '.' . $request->file('gambar')->getClientOriginalExtension();
+
+            // Upload File
+            $request->file('gambar')->move($destination, $file_name);
+
+            // Set data untuk insert ke database
+            $data['gambar'] = $destination . '/' . $file_name;
+        } else {
+            $data['gambar'] = 'noimage.png';
+        }
+
+        // Jika terdapat kode_produk yang sama, maka kembali ke halaman sebelumnya
+        if (Produk::where('kode_produk', $request->kode_produk)->count() > 0) {
+            return redirect()->back()->with('message', 'Kode Produk sudah ada');
+        } else {
+            // Jika tidak ada kode_produk yang sama, maka insert data
+            Produk::insert($data);
+            return redirect()->route('admin.products')->with('message', 'Data berhasil ditambahkan');
+        }
+
         Produk::insert($data);
-        return redirect()->route('products')->with('success', 'Data berhasil ditambahkan');
+        return redirect()->route('admin.products')->with('message', 'Data berhasil ditambahkan');
     }
 
     // Delete Data
@@ -57,19 +81,37 @@ class ProdukController extends Controller
     // Update Data
     public function update(Request $request, $id)
     {
+        // Ambil data user yang login
+        $user = $request->user();
+
         $data = [
-            'category_id'         => $request->category_id,
+            'category_id'           => $request->category_id,
             'kode_produk'           => $request->kode_produk,
             'nama_produk'           => $request->nama_produk,
-            'gambar'              => Produk::find($id)->gambar,
+            'gambar'                => Produk::find($id)->gambar,
             'produk_description'    => $request->produk_description,
             'diskon'                => $request->diskon,
             'harga_jual'            => $request->harga_jual,
             'stok'                  => $request->stok,
         ];
 
+        // Jika terdapat kode_produk yang sama, maka kembali ke halaman sebelumnya
+        if (Produk::where('kode_produk', $request->kode_produk)->where('id_produk', '!=', $id)->count() > 0) {
+            return redirect()->back()->with('message', 'Kode Produk sudah ada');
+        } else {
+            // Jika terdapat gambar yang diupload, maka gunakan gambar yang diupload
+            if ($request->hasFile('gambar')) {
+                $data['gambar'] = $request->file('gambar')->storeAs('', basename($request->file('gambar')->getClientOriginalName()));
+            }
+        }
+
         Produk::updateData($id, $data);
-        return redirect()->route('admin.products')->with('success', 'Data berhasil diupdate');
+
+        if (auth()->user()->role === 'admin') {
+            return redirect()->route('admin.products')->with('message', 'Data berhasil diupdate');
+        } else {
+            return redirect()->route('sales.products')->with('message', 'Data berhasil diupdate');
+        }
     }
 
     // Count Data Produk
